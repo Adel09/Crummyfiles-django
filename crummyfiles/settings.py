f9ac2cd22f2d68749fcb45c4f8a40a0ec7bd6bb4
Adel09 <adelnehikhare@gmail.com>
2021-09-09 11:59:06 +0100

    update

--- crummyfiles/settings.py
+++ crummyfiles/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +144,5 @@ import dj_database_url
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
