32885cdb88402c1de15344d59b3a34c218d9496d
Adel09 <adelnehikhare@gmail.com>
2021-09-09 11:24:15 +0100

    update

--- crummyfiles/settings.py
+++ crummyfiles/settings.py
@@ -135,3 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+# New database config
+import dj_database_url
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
