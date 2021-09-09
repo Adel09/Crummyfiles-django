8141484f23ac9783b9c298f26aab6698b9c3bf76
Adel09 <adelnehikhare@gmail.com>
2021-09-09 12:14:51 +0100

    update

--- crummyfiles/settings.py
+++ crummyfiles/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$62ctnbv^-)!%lvbwvj&$v#!asfw=*8csa8d&zuir+f@o3rq+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
